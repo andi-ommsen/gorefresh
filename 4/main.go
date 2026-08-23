099a10ee25e80a1e951c2c6fe64ad889d8d42c85
Jens Wodrich <119030332+andi-ommsen@users.noreply.github.com>
2026-08-23 19:17:20 +0200

    LESSON %

---
+++ 4/main.go
@@ -0,0 +1,21 @@
+package main
+
+var productPrice = mpa[string]float64{
+	"TSHIRTS":  20.00,
+	"MUG": 12.59,
+	"HAT": 18.79,
+	"BOOK": 9.99,
+}
+
+func calculateItemPrice(item string, quantity int) (float64, bool) {
+	price, exists := productPrice[item]	
+	if !exists {
+		return 0.0, false
+	}
+	return price * float64(quantity), true
+}
+
+
+func main() {
+
+}
