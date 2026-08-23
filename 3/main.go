9f805cd1f063daf94d44d87193de1f8f5e91d569
Jens Wodrich <119030332+andi-ommsen@users.noreply.github.com>
2026-08-23 07:15:00 +0200

    lesseon 4

--- 3/main.go
+++ 3/main.go
@@ -2,9 +2,63 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
+
+	// ---------------------------- IF/ELSE Statement ----------------------------
+
+	tmp := 25
+
+	// normales If/ELSE statement
+	if tmp > 20 {
+		fmt.Println("tmp is greater than 20")
+	} else {
+		fmt.Println("tmp is less than or equal to 20")
+	}
+
+	// Assoziative Arrays in PHP, in Go heißen sie "maps"
+	access := map[string]bool{
+		"admin": true,
+		"user":  false,
+	}
+
+	// Initialisierung und Prüfung in einem Schritt
+	if ok := access["admin"]; ok {
+		fmt.Println("Access granted for admin")
+	} else {
+		fmt.Println("Access denied for admin")
+	}
+
+	// ---------------------------- SWITCH Statement ----------------------------
+
+	day := "Monday"
+
+	switch day {
+	case "Monday":
+		fmt.Println("It's Monday")
+	case "Tuesday":
+		fmt.Println("It's Tuesday")
+	default:
+		fmt.Println("It's another day")
+	}
+
+	typeCheck := func(i interface{}) {
+		switch v := i.(type) {
+		case int:
+			fmt.Println("Type is int:", v)
+		case string:
+			fmt.Println("Type is string:", v)
+		case bool:
+			fmt.Println("Type is bool:", v)
+		default:
+			fmt.Println("Unknown type")
+		}
+
+	}
+	typeCheck(42)
+	typeCheck("Hello, Go!")
+	typeCheck(true)
+	typeCheck(3.14) // Unknown type
 
 }
