a3ce0d796ba99529c96d5a1d95dae089b9d1df8a
Jens Wodrich <jens.wodrich@skon.de>
2026-08-24 10:40:26 +0200

    Warenkorb

--- 4/main.go
+++ 4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 var productPrice = map[string]float64{
@@ -11,14 +12,40 @@ var productPrice = map[string]float64{
 	"BOOK":    9.99,
 }
 
-func calculateItemPrice(item string, quantity int) (float64, bool) {
-	price, exists := productPrice[item]
-	if !exists {
+func calculateItemPrice(itemCode string) (float64, bool) {
+	basePrice, found := productPrice[itemCode]
+	if !found {
+		if strings.HasSuffix(itemCode, "_SALE") {
+			originalItemCode := strings.TrimSuffix(itemCode, "_SALE")
+			basePrice, found = productPrice[originalItemCode]
+			if found {
+				salePrice := basePrice * 0.8 // Apply 20% discount for sale items
+				fmt.Printf("Item %s is on sale! Original price: %.2f, Sale price: %.2f\n", originalItemCode, basePrice, salePrice)
+
+				return salePrice, true
+			}
+
+			fmt.Printf("Item %s not found in product list.\n", originalItemCode)
+			return 0.0, false
+		}
+
 		return 0.0, false
 	}
-	return price * float64(quantity), true
+
+	return basePrice, true
 }
 
 func main() {
-	fmt.Println(calculateItemPrice("TSHIRTS", 2))
+	orderItems := []string{"TSHIRTS", "MUG", "HAT_SALE", "HAT",}
+	var totalPrice float64 
+	for _, item := range orderItems {
+		price, found := calculateItemPrice(item)
+		if found {
+			totalPrice += price
+		} else {
+			fmt.Printf("Item %s not found in product list.\n", item)
+		}
+		
+	}
+    fmt.Printf("Total price for the order: %.2f\n", totalPrice)
 }
