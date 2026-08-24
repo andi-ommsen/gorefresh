4d528023c0ecf9b4262659f44308e1b0bcb8297e
Jens Wodrich <jens.wodrich@skon.de>
2026-08-24 10:01:48 +0200

    firma

--- 4/main.go
+++ 4/main.go
@@ -1,21 +1,24 @@
 package main
 
-var productPrice = mpa[string]float64{
-	"TSHIRTS":  20.00,
-	"MUG": 12.59,
-	"HAT": 18.79,
-	"BOOK": 9.99,
+import (
+	"fmt"
+)
+
+var productPrice = map[string]float64{
+	"TSHIRTS": 20.00,
+	"MUG":     12.59,
+	"HAT":     18.79,
+	"BOOK":    9.99,
 }
 
 func calculateItemPrice(item string, quantity int) (float64, bool) {
-	price, exists := productPrice[item]	
+	price, exists := productPrice[item]
 	if !exists {
 		return 0.0, false
 	}
 	return price * float64(quantity), true
 }
 
-
 func main() {
-
+	fmt.Println(calculateItemPrice("TSHIRTS", 2))
 }
--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
-# this is  arepo just for saving my progress in my go referesh course. 
+# this is  a repo just for saving my progress in my go referesh course. 
 
 ## basics
+
+### 1-4 basics 
+
+### 5
 
